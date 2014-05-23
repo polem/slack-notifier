60f3af23307deeeb3c97e6e7c41ae44b133fee11
mteneur <maxime.teneur@lanetscouade.com>
2014-05-23 10:58:33 +0200

    Add a cli bin to slack notifier

---
+++ tests/Slack/Test/Command/NotifierCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Slack\Test\Command;
+use Slack\Command\NotifierCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class NotifierCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        $application = new Application();
+        $application->add(new NotifierCommand());
+
+        $command = $application->find('notify');
+        $commandTester = new CommandTester($command);
+        try {
+            $commandTester->execute(array('command' => $command->getName(), 'message'=>'test','channel'=>'test'));
+            $this->failed('');
+
+        } catch (Guzzle\Http\Exception\CurlException $expected) {
+            return;
+        }
+
+    }
+}
